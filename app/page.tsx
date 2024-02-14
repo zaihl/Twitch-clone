f9e37a00db09f815915987c7d97113e72779c494
zaihl <s.ingole@email.com>
2024-02-14 11:39:51 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      Hello World
+      <p className="text-red-500 font-bold">hello world</p>
     </main>
   );
 }
